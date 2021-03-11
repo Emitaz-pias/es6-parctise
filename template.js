7e9a3a01af497cb2d01f42a66f355a425a42db6d
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-03-11 06:58:48 +0600

    how to make templte with carrate

---
+++ template.js
@@ -0,0 +1,14 @@
+//how to make add a string ... template before es6
+const  name1 = "Abdur Rahim";
+const name2 = "Abdullah";
+const names = name1 + " "+name2+ "is a good boy";
+console.log(names);
+// after es6
+const names2 = `${name1} ${name2} is a good boy   `
+console.log(names2);
+// multiline 
+const  letter = `hi there
+how ar eyou
+ logn time no see :()`
+ console.log( letter);
+
